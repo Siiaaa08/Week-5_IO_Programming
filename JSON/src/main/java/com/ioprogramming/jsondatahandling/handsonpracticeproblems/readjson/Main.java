507daf09e1bf945568b261bd8ecd4686a454cc1b
Siiaaa08 <salonibaruaa810@gmail.com>
2025-02-25 10:53:30 -0800

    [Saloni]Add  Read a JSON file and print all keys and values

---
+++ JSON/src/main/java/com/ioprogramming/jsondatahandling/handsonpracticeproblems/readjson/Main.java
@@ -0,0 +1,30 @@
+package com.ioprogramming.jsondatahandling.handsonpracticeproblems.readjson;
+
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+
+    public class Main {
+        public static void main(String[] args) {
+            try {
+                ObjectMapper objectMapper = new ObjectMapper();
+
+                JsonNode rootNode = objectMapper.readTree(new File("sample.json"));
+
+                JsonNode dataArray = rootNode.get("data");
+
+                for (JsonNode node : dataArray) {
+                    String name = node.get("name").asText();
+                    String email = node.get("email").asText();
+                    System.out.println("Name: " + name + ", Email: " + email);
+                }
+
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+    }
+
+
