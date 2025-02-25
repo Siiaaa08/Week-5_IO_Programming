86cb792203cb264de5df6c8d6d60994a5ca7cc0d
Siiaaa08 <salonibaruaa810@gmail.com>
2025-02-25 10:58:05 -0800

    [Saloni]Add Validate an email field using JSON Schema

---
+++ JSON/src/main/java/com/ioprogramming/jsondatahandling/handsonpracticeproblems/validateemail/EmailValidator.java
@@ -0,0 +1,55 @@
+package com.ioprogramming.jsondatahandling.handsonpracticeproblems.validateemail;
+
+    import com.fasterxml.jackson.databind.JsonNode;
+    import com.fasterxml.jackson.databind.ObjectMapper;
+
+    import java.io.FileNotFoundException;
+    import java.io.FileReader;
+    import java.io.IOException;
+
+    import com.fasterxml.jackson.databind.JsonNode;
+    import com.fasterxml.jackson.databind.ObjectMapper;
+    import org.everit.json.schema.Schema;
+    import org.everit.json.schema.loader.SchemaLoader;
+    import org.json.JSONObject;
+
+    public class EmailValidator {
+        public static void main(String[] args) {
+            try {
+                String schemaStr = """
+                {
+                  "$schema": "https://json-schema.org/draft/2020-12/schema",
+                  "type": "object",
+                  "properties": {
+                    "email": {
+                      "type": "string",
+                      "format": "email"
+                    }
+                  },
+                  "required": ["email"]
+                }
+                """;
+
+                String jsonStr = """
+                {
+                  "email": "ujjwalg@gmail.com"
+                }
+                """;
+
+                ObjectMapper objectMapper = new ObjectMapper();
+                JsonNode jsonNode = objectMapper.readTree(jsonStr);
+                JSONObject jsonSchema = new JSONObject(schemaStr);
+                JSONObject jsonData = new JSONObject(jsonNode.toString());
+
+                Schema schema = SchemaLoader.load(jsonSchema);
+                schema.validate(jsonData);
+
+                System.out.println("Email is valid!");
+
+            } catch (Exception e) {
+                System.out.println("Invalid email: " + e.getMessage());
+            }
+        }
+    }
+
+
